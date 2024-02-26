3993472ca1cff2bc03e4581158d1c10a741c9402
Renan Hatherly <renan.hath@gmail.com>
2024-02-26 11:17:16 -0300

    Implementation of calculator with addition operation
    First implementation of the calculator, firstly with sum operations.

---
+++ RESTWithNET8/Controllers/CalculatorController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RESTWithNET8.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        // +
+        [HttpGet("addition/{firstNumber}/{secNumber}")]
+        public IActionResult Get(string firstNumber, string secNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secNumber))
+            {
+                var addition = ConvertToDecimal(firstNumber) + ConvertToDecimal(secNumber);
+
+                return Ok(addition.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any,
+                                            System.Globalization.NumberFormatInfo.InvariantInfo, out number); 
+
+            return isNumber;
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalNumber;
+
+            if (decimal.TryParse(strNumber, out decimalNumber))
+            {
+                return decimalNumber;
+            }
+
+            return 0;
+        }
+    }
+}
