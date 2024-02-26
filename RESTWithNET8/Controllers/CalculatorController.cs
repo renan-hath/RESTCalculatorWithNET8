46c8d67011f38944d12f85ec9828f00434fb58c2
Renan Hatherly <renan.hath@gmail.com>
2024-02-26 12:05:39 -0300

    Basic math operations, mean and squareroot implemented
    - Subtraction, multiplication, division, squareroot and mean have been implemented.
    - A few routes and launch settings modifications

--- RESTWithNET8/Controllers/CalculatorController.cs
+++ RESTWithNET8/Controllers/CalculatorController.cs
@@ -13,15 +13,79 @@ namespace RESTWithNET8.Controllers
             _logger = logger;
         }
 
-        // +
-        [HttpGet("addition/{firstNumber}/{secNumber}")]
-        public IActionResult Get(string firstNumber, string secNumber)
+        [HttpGet("add/{firstNumber}/{secondNumber}")]
+        public IActionResult Add(string firstNumber, string secondNumber)
         {
-            if (IsNumeric(firstNumber) && IsNumeric(secNumber))
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
             {
-                var addition = ConvertToDecimal(firstNumber) + ConvertToDecimal(secNumber);
+                var addition = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
 
                 return Ok(addition.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("subtract/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtract(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var subtraction = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+
+                return Ok(subtraction.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("multiply/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiply(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var multiplication = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+
+                return Ok(multiplication.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("divide/{firstNumber}/{secondNumber}")]
+        public IActionResult Divide(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var division = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+
+                return Ok(division.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("mean/{firstNumber}/{secondNumber}")]
+        public IActionResult Mean(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var mean = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+
+                return Ok(mean.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("squareroot/{number}")]
+        public IActionResult SquareRoot(string number)
+        {
+            if (IsNumeric(number))
+            {
+                var squareRoot = Math.Sqrt(ConvertToDouble(number));
+
+                return Ok(squareRoot.ToString());
             }
 
             return BadRequest("Invalid input");
@@ -36,13 +100,25 @@ namespace RESTWithNET8.Controllers
             return isNumber;
         }
 
-        private decimal ConvertToDecimal(string strNumber)
+        private decimal ConvertToDecimal(string stringNumber)
         {
             decimal decimalNumber;
 
-            if (decimal.TryParse(strNumber, out decimalNumber))
+            if (decimal.TryParse(stringNumber, out decimalNumber))
             {
                 return decimalNumber;
+            }
+
+            return 0;
+        }
+
+        private double ConvertToDouble(string stringNumber)
+        {
+            double doubleNumber;
+
+            if (double.TryParse(stringNumber, out doubleNumber))
+            {
+                return doubleNumber;
             }
 
             return 0;
